2082e75c7ea5952c12dab414e779c1ccab2f8a8c
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2023-12-23 11:58:05 +0530

    settings.py uploaded
    Initial commit

---
+++ src/settings.py
@@ -0,0 +1,3 @@
+def init():
+    global fcn, fcl, fdl, fpl, fpn, cat_names, cat_links, done_links, page_links, page_names
+    fcn, fcl, fdl, fpl, fpn, cat_names, cat_links, done_links, page_links, page_names = None, None, None, None, None, None, None, None, None, None
