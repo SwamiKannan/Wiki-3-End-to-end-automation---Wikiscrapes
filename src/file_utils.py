5b21d17b37d97ed80951a4bf391f98fcabd53bcf
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2023-12-23 12:30:53 +0530

    Update file_utils.py
    Updated status update of files

--- src/file_utils.py
+++ src/file_utils.py
@@ -41,62 +41,60 @@ def open_files(primary_url, output_dir):
     if os.path.exists(os.path.join(output_dir, 'category_names.txt')):
         settings.fcn = open(os.path.join(output_dir, 'category_names.txt'), 'r+', encoding='utf-8')
         settings.cat_names = set([t.replace('\n', '') for t in settings.fcn.readlines() if t != '\n'])
-        print(f'Categories file exists. {len(settings.cat_names)} categories included')
+        print(f'Categories file exists. Opening....\n {len(settings.cat_names)} categories included')
 
     else:
         settings.fcn = open(os.path.join(output_dir, 'category_names.txt'), 'a+', encoding='utf-8')
-        print('Categories file does not exist')
+        print('Categories file does not exist. Creating.....')
         settings.cat_names = set()
 
     if os.path.exists(os.path.join(output_dir, 'category_links.txt')):
         settings.fcl = open(os.path.join(output_dir, 'category_links.txt'), 'r+', encoding='utf-8')
         settings.cat_links = set([t.replace('\n', '') for t in settings.fcl.readlines() if t != '\n'])
-        print(f'Category links file exists. {len(settings.cat_links)} category links included')
+        print(f'Category links file exists. Opening....\n {len(settings.cat_links)} category links included')
 
     else:
         settings.fcl = open(os.path.join(output_dir, 'category_links.txt'), 'a+', encoding='utf-8')
-        print('Category links file does not exist')
+        print('Category links file does not exist. Creating.....')
         settings.cat_links = set()
         settings.cat_links.add(primary_url)
 
     if os.path.exists(os.path.join(output_dir, 'done_links.txt')):
         settings.fdl = open(os.path.join(output_dir, 'done_links.txt'), 'r+', encoding='utf-8')
         settings.done_links = set([t.replace('\n', '') for t in settings.fdl.readlines() if t != '\n'])
-        print(f'Done links file exists. {len(settings.done_links)} links processing completed')
+        print(f'Done links file exists. Opening....\n {len(settings.done_links)} links processing completed')
 
     else:
         settings.fdl = open(os.path.join(output_dir, 'done_links.txt'), 'a+', encoding='utf-8')
-        print('Done links file does not exist')
+        print('Done links file does not exist. Creating.....')
         settings.done_links = set()
 
     if os.path.exists(os.path.join(output_dir, 'page_links.txt')):
         settings.fpl = open(os.path.join(output_dir, 'page_links.txt'), 'r+', encoding='utf-8')
         settings.page_links = set([t.replace('\n', '') for t in settings.fpl.readlines() if t != '\n'])
-        print(f'Page links file exists. {len(settings.page_links)} page links included')
+        print(f'Page links file exists. Opening....\n {len(settings.page_links)} page links included')
     else:
         settings.fpl = open(os.path.join(output_dir, 'page_links.txt'), 'a+', encoding='utf-8')
-        print('Page links file does not exist')
+        print('Page links file does not exist. Creating.....')
         settings.page_links = set()
 
     if os.path.exists(os.path.join(output_dir, 'page_names.txt')):
         settings.fpn = open(os.path.join(output_dir, 'page_names.txt'), 'r+', encoding='utf-8')
         settings.page_names = set([t.replace('\n', '') for t in settings.fpn.readlines() if t != '\n'])
-        print(f'Page names file exists. {len(settings.page_names)} pages included')
+        print(f'Page names file exists. Opening....\n {len(settings.page_names)} pages included')
     else:
         settings.fpn = open(os.path.join(output_dir, 'page_names.txt'), 'a+', encoding='utf-8')
-        print('Page name file does not exist')
+        print('Page name file does not exist. Creating.....')
         settings.page_names = set()
 
     print('\n\n\n')
     return output_dir
 
 
-
 def initiate_file_opens(output_dir, primary_url):
     output_paths = check_paths(output_dir)
     output_dirs = open_files(primary_url, output_paths)
     return output_dirs
-
 
 
 def write_files():
