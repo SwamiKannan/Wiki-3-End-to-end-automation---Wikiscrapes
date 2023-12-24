c2a763cb2b945a4b6d0563dc99751dba080fd159
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2023-12-24 11:52:54 +0530

    Update settings.py
    Added queue lengths for global references

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,6 @@
 def init():
     global fcn, fcl, fdl, fpl, fpn, cat_names, cat_links, done_links, page_links, page_names
     fcn, fcl, fdl, fpl, fpn, cat_names, cat_links, done_links, page_links, page_names = None, None, None, None, None, None, None, None, None, None
+    global MAXSIZE_EPNQ, EOQ_COUNT
+    MAXSIZE_EPNQ = 0
+    EOQ_COUNT = 0
