4cee1656374e03d4e9709f2eaaa7d547f3ff1c6e
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2023-12-24 12:07:03 +0530

    Update settings.py
    Added text files list (Currently [] but is updated in the scrape_wikicategory.py as the list of names of text files already processed. The get_pages functions checks if the page names does not exist in the drive before putting its output on the queue. It does that by referring to the settings.text_files list

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,8 @@
 def init():
     global fcn, fcl, fdl, fpl, fpn, cat_names, cat_links, done_links, page_links, page_names
     fcn, fcl, fdl, fpl, fpn, cat_names, cat_links, done_links, page_links, page_names = None, None, None, None, None, None, None, None, None, None
-    global MAXSIZE_EPNQ, EOQ_COUNT
+    global MAXSIZE_EPNQ, EOQ_COUNT, text_files, count_files
     MAXSIZE_EPNQ = 0
     EOQ_COUNT = 0
+    text_files = []
+    count_files = 0
